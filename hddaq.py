52be6c70030cedfee44f92cb01efdccd9dce57e5
parkeuge <parkeuge@usc.edu>
2020-06-30 03:40:03 -0700

    Modified exisiting files

--- hddaq.py
+++ hddaq.py
@@ -9,7 +9,10 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-
+AVI_DEVICES = ('Device 1', 'Device 2', 'Device 3')
+THERMAL_DEVICES = ('Device 1', 'Device 2', 'Device 3')
+A_O_DEVICES = ('Device 1', 'Device 2', 'Device 3')
+D_I_O_DEVICES = ('Device 1', 'Device 2', 'Device 3')
 analog_voltage_input = [1,2,3,4,5,6,7,8]
 thermocouple_input = [1,2,3,4]
 analog_output = [1,2]
@@ -17,7 +20,7 @@ digital_i_o = [1,2,3,4,5,6,7,8]
 results_chart = ["Q Factor","Thrust","Thrust/P"]
 counter = 0
 r0 = 5
-r1 = 5
+r1 = 2
 r2 = 5
 r3 = 5
 r4 = 8
@@ -58,33 +61,50 @@ def readFile(self,filename):
     with open(filename,"r") as f:
         text = f.read()
     return text
+    
+    
+def get_118_info():
+    stat_118 = Toplevel()
+    stat_118.title("MCC 118")
+    #info_118 = mcc118.info()
+    #firmware_version_118 = mcc118.firmware_version()
+    #serial_118 = mcc118.serial()
+    
+def get_134_info():
+    stat_134 = Toplevel()
+    stat_134.title("MCC 134")
+    #info_134 = mcc134.info()
+    #firmware_version_134 = mcc134.firmware_version()
+    #serial_134 = mcc134.serial()
+
+def get_152_info():
+    stat_152 = Toplevel()
+    stat_152.title("MCC 152")
+    #info_152 = mcc152.info()
+    #firmware_virsion_152 = mcc152.firmware_version()
+    #serial_152 = mcc152.serial()
+
+
+
         
 class NoiseWindow(Toplevel):
-    def __init__(self,master=None):
-        super().__init__(master=master)
+    def __init__(self,parent=None,side=LEFT):
+        super().__init__()
         self.title("External Noise Input Menu")
-        self.geometry("400x400")
-        Label(self, text ="Check any factor that may have contributed to noise:").grid(row=0)
-        chkbtn1 = ttk.Checkbutton(self, text = "No external noise").grid(row=1)
-        chkbtn2 = ttk.Checkbutton(self, text = "Air Currents (Atmospheric Test)").grid(row=2)
-        chkbtn3 = ttk.Checkbutton(self, text = "RF Interaction with the Surrounding Environment").grid(row=3)
-        chkbtn4 = ttk.Checkbutton(self, text = "Magnetic Interaction").grid(row=4)
-        chkbtn5 = ttk.Checkbutton(self, text = "Thermal Expansion and Contraction").grid(row=5)
-        chkbtn6 = ttk.Checkbutton(self, text = "Vibration").grid(row=6)
-        chkbtn7 = ttk.Checkbutton(self, text = "Electrostatic Interaction").grid(row=7)
-        chkbtn8 = ttk.Checkbutton(self, text = "Outgassing").grid(row=8)
-        chkbtn9 = ttk.Checkbutton(self, text = "Photon Rocket Force").grid(row=9)
-        chkbtn10 = ttk.Checkbutton(self, text = "Impulsive/Thermal Signal Decoupling Error").grid(row=10)
+        self.geometry("350x300")
+        self.configure(background = "green" )
+        Label(self, text ="Check any factor that may have contributed to noise:").grid(row=0,sticky=W+E)
+        chkbtn1 = Checkbutton(self, text = "No external noise").grid(row=2,sticky=W)
+        chkbtn2 = Checkbutton(self, text = "Air Currents (Atmospheric Test)").grid(row=3,sticky=W)
+        chkbtn3 = Checkbutton(self, text = "RF Interaction with the Surrounding Environment").grid(row=4,sticky=W)
+        chkbtn4 = Checkbutton(self, text = "Magnetic Interaction").grid(row=5,sticky=W)
+        chkbtn5 = Checkbutton(self, text = "Thermal Expansion and Contraction").grid(row=6,sticky=W)
+        chkbtn6 = Checkbutton(self, text = "Vibration").grid(row=7,sticky=W)
+        chkbtn7 = Checkbutton(self, text = "Electrostatic Interaction").grid(row=8,sticky=W)
+        chkbtn8 = Checkbutton(self, text = "Outgassing").grid(row=9,sticky=W)
+        chkbtn9 = Checkbutton(self, text = "Photon Rocket Force").grid(row=10,sticky=W)
+        chkbtn10 = Checkbutton(self, text = "Impulsive/Thermal Signal Decoupling Error").grid(row=11,sticky=W)
  
-#class file_opener(Toplevel):
-#    def __init__(self,master=None):
-#        super().__init__(master=master)
-#
-#        self.init_file_opener()
-#
-#    def init_file_opener(self):
-#        self.title("Open File")
-#
 class colorpicker(Toplevel):
     def __init__(self,master=None):
         super().__init__(master=master)
@@ -108,15 +128,26 @@ class NewWindow(Toplevel):
         
     def graph_settings(self):
         self.title("Graph settings")
-        self.geometry("300x300")
+        self.geometry("400x400")
         graph_settings_tab = ttk.Notebook(self)
         
         general_settings = ttk.Frame(graph_settings_tab)
         filters = ttk.Frame(graph_settings_tab)
 
-        graph_settings_tab.add(general_settings, text="General Settings")
-        graph_settings_tab.add(filters, text= "Filters")
+        graph_settings_tab.add(filters, text= "Graph Filters")
+        graph_settings_tab.add(general_settings, text="Graph Settings")
         graph_settings_tab.grid(row=0)
+        
+        chkbtn1 = Checkbutton(filters, text = "No external noise").grid(row=2,sticky=W)
+        chkbtn2 = Checkbutton(filters, text = "Air Currents (Atmospheric Test)").grid(row=3,sticky=W)
+        chkbtn3 = Checkbutton(filters, text = "RF Interaction with the Surrounding Environment").grid(row=4,sticky=W)
+        chkbtn4 = Checkbutton(filters, text = "Magnetic Interaction").grid(row=5,sticky=W)
+        chkbtn5 = Checkbutton(filters, text = "Thermal Expansion and Contraction").grid(row=6,sticky=W)
+        chkbtn6 = Checkbutton(filters, text = "Vibration").grid(row=7,sticky=W)
+        chkbtn7 = Checkbutton(filters, text = "Electrostatic Interaction").grid(row=8,sticky=W)
+        chkbtn8 = Checkbutton(filters, text = "Outgassing").grid(row=9,sticky=W)
+        chkbtn9 = Checkbutton(filters, text = "Photon Rocket Force").grid(row=10,sticky=W)
+        chkbtn10 = Checkbutton(filters, text = "Impulsive/Thermal Signal Decoupling Error").grid(row=11,sticky=W)
         
     def daq_settings(self):
         self.title("Program Settings")
@@ -151,9 +182,9 @@ class menubar(Frame):
         file.add_command(label="Exit",command=None)
         
         status_menu = Menu(file)
-        status_menu.add_command(label="MCC 118",command=None)
-        status_menu.add_command(label='MCC 152', command=None)
-        status_menu.add_command(label='MCC 134', command=None)
+        status_menu.add_command(label="MCC 118",command=get_118_info)
+        status_menu.add_command(label='MCC 152', command=get_152_info)
+        status_menu.add_command(label='MCC 134', command=get_134_info)
         file.add_cascade(label='Get Shield Info',menu=status_menu)
         
         save_as_menu = Menu(file)
@@ -198,6 +229,10 @@ class menubar(Frame):
         gray2_btn = Button(toolbar, text ="gray25", relief=RAISED,bitmap="gray25").grid(row=0,column=7)
         gray3_btn = Button(toolbar, text ="gray50", relief=RAISED,bitmap="gray50").grid(row=0,column=8)
         gray4_btn = Button(toolbar, text ="gray75", relief=RAISED,bitmap="gray75").grid(row=0,column=9)
+        gray5_btn = Button(toolbar, text ="gray12", relief=RAISED,bitmap="gray12").grid(row=0,column=10)
+        gray6_btn = Button(toolbar, text ="gray25", relief=RAISED,bitmap="gray25").grid(row=0,column=11)
+        gray7_btn = Button(toolbar, text ="gray50", relief=RAISED,bitmap="gray50").grid(row=0,column=12)
+        gray8_btn = Button(toolbar, text ="gray75", relief=RAISED,bitmap="gray75").grid(row=0,column=13)
         
         toolbar.grid(row=1,column=0,sticky=W)
 
@@ -213,7 +248,6 @@ def counter_label(label):
 def status_update():
     info_tuple = mcc118.info() # Return information about the shield as a named tuple
     
-
 # driver code
 if __name__ == "__main__":
     root = Tk() # Create a new GUI window
@@ -228,15 +262,16 @@ if __name__ == "__main__":
     
     test_run_buttons = PanedWindow(orient='vertical')
     test_run_buttons.grid(row=2,column=2,sticky=W+E+N+S)
-    test_start_button = Button(root,text="START",bg="green",fg="black")
+    test_start_button = Button(root,text="START",height=2,highlightcolor='light green',relief=RAISED,background='green',activebackground='red')
     test_start_button.bind("<Button>", lambda e: start_test_warning())
-    test_pause_button = Button(root,text="STOP TEST",bg="red",fg="black")
-    camera_feed_button = Button(root,text="VIEW CAMERA FEED")
-    graph_btn = Button(root,text="FILTERS")
+    test_pause_button = Button(root,text="STOP TEST",height=2,highlightcolor='red2',relief=RAISED,background='red',activebackground='yellow')
+    camera_feed_button = Button(root,text="VIEW CAMERA FEED",relief=GROOVE)
+    graph_btn = Button(root,text="GRAPH FILTERS",relief=RIDGE)
     graph_btn.bind("<Button>",lambda e: NewWindow(root).graph_settings())
+    
 
-    results_pane = Label(text="Results\n Q Factor:\n Thrust:\n Thrust/Power:\n",relief=RIDGE)
-    active_sensor_pane = Label(text="Active Sensors:\nThermocouple\n Current Sensor\n Vacuum Pressure\n Magnetic Field\n Laser\n",relief=RIDGE)
+    results_pane = Label(text="Results\n Q Factor:\n Thrust:\n Thrust/Power:",justify=LEFT,anchor=W,relief=RIDGE)
+    active_sensor_pane = Label(text="Active Sensors:\nThermocouple\n Current Sensor\n Vacuum Pressure\n Magnetic Field\n Laser",justify=LEFT,anchor=W,relief=RIDGE)
     
     pw = PanedWindow(orient='horizontal')
     pw.grid(row=3,column=3)
@@ -252,7 +287,6 @@ if __name__ == "__main__":
     test_run_buttons.add(graph_btn)
     test_run_buttons.add(camera_feed_button)
     
-    
     tab1 = ttk.Frame(tabControl)
     tab2 = ttk.Frame(tabControl)
     tab3 = ttk.Frame(tabControl)
@@ -260,51 +294,109 @@ if __name__ == "__main__":
     tabControl.add(tab1,text="MCC 118 ANALOG VOLTAGE INPUT")
     tabControl.add(tab2,text="MCC 134 THERMOCOUPLE INPUT")
     tabControl.add(tab3,text="MCC 152 ANALOG OUTPUT / DIGITAL I/O")
-
-    for channels in analog_voltage_input:
-        #ttk.Label(tab1,text=channels,relief=RIDGE).grid(row=r1,column=0,sticky=W+E+N+S)
-        ttk.Label(tab1,text=channels,).grid(row=r1,column=1,sticky=W+E+N+S)
-        avi_btn = ttk.Button(tab1,text=channels,)
-        avi_btn.grid(row=r1,column=2,sticky=W+E+N+S)
-        avi_btn.bind("<Button>",lambda e: NewWindow(root))
-        ttk.Entry(tab1,text=channels).grid(row=r1,column=3,sticky=W+E+N+S)
-        r1+=1
-    Button(tab1,text="Get Shield Info").grid(row=4,column=0)
     
+    count_label = Label(root)
+    count_label.grid(row=1,column=1)
+    counter_label(count_label)
+    
+    a = StringVar()
+    b = StringVar()
+    c = StringVar()
+    gg = StringVar()
+    ff = StringVar()
+    aa = StringVar()
+    dd = StringVar()
+    zz = StringVar()
+    gh = StringVar()
+    
+    stringvarlist = [a,b,c,gg,ff,aa,dd,zz,gh]
+    
+    temp_channel_1 = StringVar()
+    temp_channel_2 = StringVar()
+    temp_channel_3 = StringVar()
+    temp_channel_4 = StringVar()
+    temp_channel_5 = StringVar()
+    
+    temp_string_var_list = [temp_channel_1,temp_channel_2,temp_channel_3,temp_channel_4,temp_channel_5]
+    
+    analog_output_channel_1 = StringVar()
+    analog_output_channel_2 = StringVar()
+    analog_output_channel_3 = StringVar()
+    
+    analog_output_var_list = [analog_output_channel_1,analog_output_channel_2,analog_output_channel_3]
+    
+    dio_ch1 = StringVar()
+    dio_ch2 = StringVar()
+    dio_ch3 = StringVar()
+    dio_ch4 = StringVar()
+    dio_ch5 = StringVar()
+    dio_ch6 = StringVar()
+    dio_ch7 = StringVar()
+    dio_ch8 = StringVar()
+    dio_ch9 = StringVar()
+    
+    digital_output_var_list = [dio_ch1,dio_ch2,dio_ch3,dio_ch4,dio_ch5,dio_ch6,dio_ch7,dio_ch8,dio_ch9]
+    
+    ttk.Label(tab1,text="Channel Number").grid(row=0,column=0)
+    for channels in analog_voltage_input:
+        ttk.Label(tab1,text=channels).grid(row=r1,column=0,sticky=W)
+        _avi_devices = ttk.Combobox(tab1,textvariable=stringvarlist[channels],takefocus=0)
+        _avi_devices['values'] = AVI_DEVICES
+        _avi_devices.grid(row=r1,column=1)
+        avi_btn = ttk.Button(tab1,text=channels)
+        avi_btn.grid(row=r1,column=2,sticky=W)
+        avi_btn.bind("<Button>",lambda e: NewWindow(root))
+        ttk.Entry(tab1,text=channels).grid(row=r1,column=3,sticky=W)
+        r1+=1
+    avi_button = Button(tab1,text="Get Shield Info")
+    avi_button.grid(row=15,column=0)
+    avi_button.bind("<Button>",lambda e: get_118_info())
+
+    ttk.Label(tab2,text="Channel Number").grid(row=0,column=0)
     for channels in thermocouple_input:
-        ttk.Label(tab2,text=channels,relief=RIDGE).grid(row=r2,column=0)
-        ttk.Label(tab2,text=channels).grid(row=r2,column=1)
+        ttk.Label(tab2,text=channels).grid(row=r2,column=0,sticky=W)
+        temp_devices = ttk.Combobox(tab2,textvariable=temp_string_var_list[channels],takefocus=0)
+        temp_devices['values'] = THERMAL_DEVICES
+        temp_devices.grid(row=r2,column=1)
         thermo_btn = ttk.Button(tab2,text=channels)
         thermo_btn.grid(row=r2,column=2)
         thermo_btn.bind("<Button>",lambda e: NewWindow(root))
         ttk.Label(tab2,text=channels).grid(row=r2,column=3)
         r2+=1
-    Button(tab2,text="Get Shield Info").grid(row=4,column=0)
-        
+    thm_button = Button(tab2,text="Get Shield Info")
+    thm_button.grid(row=15,column=0)
+    thm_button.bind("<Button>",lambda e: get_134_info())
+    
+    ttk.Label(tab3,text="Channel Number").grid(row=0,column=0)
     for channels in analog_output:
-        ttk.Label(tab3,text=channels,relief=RIDGE).grid(row=r3,column=0)
-        ttk.Label(tab3,text=channels).grid(row=r3,column=1)
+        ttk.Label(tab3,text=channels).grid(row=r3,column=0,sticky=W)
+        _avo_devices = ttk.Combobox(tab3,textvariable=analog_output_var_list[channels],takefocus=0)
+        _avo_devices['values'] = A_O_DEVICES
+        _avo_devices.grid(row=r3,column=1)
         ttk.Entry(tab3,text=channels).grid(row=r3,column=2)
         ao_btn = ttk.Button(tab3,text=channels)
         ao_btn.grid(row=r3,column=3)
         ao_btn.bind("<Button>",lambda e: NewWindow(root))
-        ttk.Label(tab3,text=channels).grid(row=r3,column=4)
+        ttk.Label(tab3,text=channels).grid(row=r3,column=4,sticky=W)
         r3+=1
     
         
     for channels in digital_i_o:
-        ttk.Label(tab3,text=channels,relief=RIDGE).grid(row=r4,column=0)
-        ttk.Label(tab3,text=channels).grid(row=r4,column=1)
+        ttk.Label(tab3,text=channels).grid(row=r4,column=0,sticky=W)
+        digital_io_devices = ttk.Combobox(tab3,textvariable=digital_output_var_list[channels],takefocus=0)
+        digital_io_devices['values'] = D_I_O_DEVICES
+        digital_io_devices.grid(row=r4,column=1)
         dio_btn = ttk.Button(tab3,text=channels)
-        dio_btn.grid(row=r4,column=2)
+        dio_btn.grid(row=r4,column=2,sticky=W+E+N+S)
         dio_btn.bind("<Button>",lambda e: NewWindow(root))
-        ttk.Label(tab3,text=channels).grid(row=r4,column=3)
+        ttk.Label(tab3,text=channels).grid(row=r4,column=3,sticky=W)
         r4+=1
-    Button(tab3,text="Get Shield Info").grid(row=4,column=0)
-        
+    dio_button = Button(tab3,text="Get Shield Info")
+    dio_button.grid(row=20,column=0)
+    dio_button.bind("<Button>",lambda e: get_152_info())
 
 
-    noise_btn = Button(text="External Disturbance Input\n\n (Click after a test cycle is conducted if needed.)")
+    noise_btn = Button(text="External Disturbance Input\n (Click after a test cycle is conducted if needed.)")
     noise_btn.bind("<Button>",lambda e: NoiseWindow(root))
     test_run_buttons.add(noise_btn)
     test_run_buttons.add(results_pane)
@@ -321,7 +413,7 @@ if __name__ == "__main__":
     displacement_from_power = nm_W_graph.add_subplot(111,xlabel="Power (W)",ylabel="Displacement (nm)")
     em_force_as_function.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
     em_force_variations.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
-    displacement_from_power.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+    displacement_from_power.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5]) # most likely scenario would be to open a file to the csv text of the displacement data
     uN_mA_graph.suptitle("Force Produced from Current")
     #uN_mA_graph.xlabel("Current (mA)")
     #uN_mA_graph.ylabel("Force (µN)")
